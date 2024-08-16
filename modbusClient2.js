7d34b243a518c8cc29b1246ba8a8db0657317748
Francisco Gonzalez Muñoz <paco_sheamus@hotmail.com>
2024-08-16 12:05:58 -0600

    add: modbusClient2 test

--- modbusClient2.js
+++ modbusClient2.js
@@ -2,14 +2,28 @@ const ModbusRTU = require('modbus-serial');
 const client = new ModbusRTU();
 
 // Dirección IP del PLC y puerto Modbus TCP 
-// Por defecto es el 502 ???
-const plcAddress = '10.10.100.36';
-const plcPort = 502;
+const plcAddress = '10.10.106.241';
+
+// Función para convertir un valor de 16 bits sin signo.... a un valor con signo
+// En valores de 16 bits, los numeros positivos (con signo) no requieren ninguna conversion
+// Pero los valores de 16 bits sin signo (En este caso, los negativos con signo -) si requieren conversion
+// En este caso, el rango -32768 a 32767 en total son 65536, por lo que al llegar un numero negativo, se resta 655536
+
+// Este detalle adicional se presenta debido a que en Modbus, los datos se almacenan como valores enteros sin signo de 16 bits
+
+function toSigned16Bit(value) {
+    // int 	16	-32768 a 32767
+    // Si el valor es mayor o igual a 32768, es negativo en la representación de 16 bits con signo
+    if (value >= 32768) {
+        return value - 65536; // 65536 es 2^16
+    }
+    return value;
+}
 
 // Aqui se intenta conectar al PLC
 async function connect() {
     try {
-        await client.connectTCP(PLC_IP, { port: PLC_PORT });
+        await client.connectTCP(plcAddress);
         console.log('Conectado al PLC');
     } catch (error) {
         console.error('Error al conectar al PLC:', error);
@@ -20,20 +34,22 @@ async function readRegisters() {
     try {
         // Aqui se leen los registros emitidos por el PLC mediante MODBUS
         // Por ejemplo, los registros del 40001 a 40016
-        const data = await client.readInputRegisters(40001, 40016);
-        console.log('Registros leídos:', data.data);
+        const data = await client.readHoldingRegisters(0, 20);
+        // Convertir los valores leídos a enteros con signo
+        const signedData = data.data.map(toSigned16Bit);
+        console.log(`Registros leídos desde ${plcAddress} MODBUS:`, signedData);
     } catch (error) {
         console.error('Error al leer los registros:', error);
     }
 }
 
-// Función para hacer lecturas periódicas cada 10 segundos
+// Función para hacer lecturas cada 10 segundos
 async function startReading() {
     await connect();
 
     setInterval(async () => {
         await readRegisters();
-    }, 10000); // 10000 ms, eqiovale a 10 segundos
+    }, 8000); // 8000 ms, equivale a 8 segundos
 }
 
 // Ejecutar la función de MODBUS startReading
---
+++ modbusClient3.js
@@ -0,0 +1,3 @@
+// 1.- Conectarnos al PLC
+// 2.- Hacer una funcion que lea los registros
+// 3.- Hacer una funcions
